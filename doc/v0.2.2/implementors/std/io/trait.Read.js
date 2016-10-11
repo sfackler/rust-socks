de7d77a81e9774200840d21a9f3e632d594bfeae
Steven Fackler <sfackler@gmail.com>
2016-10-11 14:30:59 -0700

    Release v0.2.2

---
+++ doc/v0.2.2/implementors/std/io/trait.Read.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["socks"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.Read.html' title='std::io::Read'>Read</a> for <a class='struct' href='socks/struct.Socks4Stream.html' title='socks::Socks4Stream'>Socks4Stream</a>","impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.Read.html' title='std::io::Read'>Read</a> for &amp;'a <a class='struct' href='socks/struct.Socks4Stream.html' title='socks::Socks4Stream'>Socks4Stream</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.Read.html' title='std::io::Read'>Read</a> for <a class='struct' href='socks/struct.Socks5Stream.html' title='socks::Socks5Stream'>Socks5Stream</a>","impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.Read.html' title='std::io::Read'>Read</a> for &amp;'a <a class='struct' href='socks/struct.Socks5Stream.html' title='socks::Socks5Stream'>Socks5Stream</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
